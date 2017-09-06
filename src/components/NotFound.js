934b3803c29d09fe31deb8d31e70765770300943
Clement Debroize <clement.debroize@gmail.com>
2017-09-06 19:08:50 +0200

    Mise en place de la base

---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <p className="not_found">Il me semble que vous vous êtes perdu !</p>
+    )
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,25 @@
+// Base
+import React from 'react';
+import {render} from 'react-dom';
+// Router
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+// Components
+import Home from './components/Home';
+import NotFound from './components/NotFound';
+// CSS
+import './style.css';
+
+const Root = () => {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route component={NotFound}/>
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+render(
+  <Root/>, document.getElementById('root'));
